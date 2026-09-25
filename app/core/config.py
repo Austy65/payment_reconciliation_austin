5e2d71a73fb5679490dfb7f8018a14fc79e806e3
Austin Quadras <austinquadras@gmail.com>
2026-09-26 01:19:25 +0530

    Deploy application

--- app/core/config.py
+++ app/core/config.py
@@ -1,13 +1,29 @@
+# from pydantic_settings import BaseSettings
+
+
+# class Settings(BaseSettings):
+#     DATABASE_URL: str = "postgresql://postgres:postgres@localhost:5432/setu_recon"
+#     APP_ENV: str = "development"
+#     PAGE_SIZE_DEFAULT: int = 20
+#     PAGE_SIZE_MAX: int = 100
+
+#     model_config = {"env_file": ".env", "extra": "ignore"}
+
+
+# settings = Settings()
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql://postgres:postgres@localhost:5432/setu_recon"
+    DATABASE_URL: str
     APP_ENV: str = "development"
     PAGE_SIZE_DEFAULT: int = 20
     PAGE_SIZE_MAX: int = 100
 
-    model_config = {"env_file": ".env", "extra": "ignore"}
+    model_config = {
+        "env_file": ".env",
+        "extra": "ignore",
+    }
 
 
 settings = Settings()
